a1999bfe19d8f51a1158a4998a38895e70b251be
Teodoro Salgado Mac <tsalgado96@gmail.com>
2017-06-07 20:32:22 -0400

    Single pages now display the page title.

---
+++ page.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+    <div class="container">
+
+      <div class="blog-header">
+        <h1 class="blog-title"><?php echo get_the_title(); ?></h1>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-8 blog-main">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <div class="blog-post">
+            <?php the_content(); ?>
+          </div><!-- /.blog-post -->
+        <?php endwhile; else : ?>
+          <h1><?php _e( 'Sorry, no page found.' ); ?></h1>
+        <?php endif; ?>
+        </div><!-- /.blog-main -->
+
+        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+          <div class="sidebar-module sidebar-module-inset">
+            <h4><?php echo get_theme_mod('community_sidebar_heading', 'About'); ?></h4>
+            <p><?php echo get_theme_mod('community_sidebar_text', 'Place description here'); ?></p>
+          </div>
+          <?php if(is_active_sidebar('sidebar')): ?>
+          <?php dynamic_sidebar('sidebar'); ?>
+          <?php endif; ?>
+        </div><!-- /.blog-sidebar -->
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+<?php get_footer(); ?>
