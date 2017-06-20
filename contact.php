c71a583a09b8bff62b647bb68b018f0c1feb6b67
Teodoro Salgado Mac <tsalgado96@gmail.com>
2017-06-20 01:01:11 -0400

    Made a main.js file. Added Google Map location of Madagascar to Contact Us page.

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php /* Template Name: Contact Page */ ?>
+<?php get_header(); ?>
+<div id="contactHeroImage">
+  <div class="container-fluid aboutOverlay">
+    <div class="aboutHero">
+      <h1 class="aboutHeroHeading"><span class="whiteBorder">Contact Us</span></h1>
+    </div>
+  </div>
+</div>
+
+<div class="about-offwhite-bg">
+  <div class="container contact-container text-center">
+    <div class="row">
+
+      <div class="col-xs-12 mediumTopPadding mediumBottomPadding">
+        <h2 class="contactHeading">Mickie Nagel</h2>
+        <p class="contactSubText"><a href="mailto:mickie@belocalgrp.com" target="_top">mickie@belocalgrp.com</a></p>
+      </div>
+
+    </div>
+  </div>
+  <div class="mapOverlay mapOverlayActive" onClick="style.pointerEvents='none'"></div>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7739106.085288365!2d42.338880904017024!3d-18.715067408349096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x21d1a4e3ea238545%3A0x5244e3c1977b1388!2sMadagascar!5e0!3m2!1sen!2sus!4v1497917726888" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+<?php get_footer(); ?>
